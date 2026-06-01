eff6f4d2b0400550dae022b839b891d7bb235dbd
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-06-01 09:07:24 -0300

    docs: Comentário explicando a funcionalidade desta página dentro do código.

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -9,3 +9,6 @@ CREATE TABLE usuario (
 );
 
 INSERT INTO usuario (usuario, senha) VALUE ('admin', '123');
+
+
+--Esta página serve para criar o banco de dados e criar uma tabela de usuário e inserir um  usúario a está tabela no banco de dados.
