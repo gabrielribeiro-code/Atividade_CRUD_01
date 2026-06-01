435f40fc8828cb358cea4b2d1fbc1dc272932bf0
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-06-01 09:27:45 -0300

    docs: Comentários explicando toda a funcionalidade da página home dentro do nosso pequeno sistema para logar alguém.

--- public/home.php
+++ public/home.php
@@ -5,18 +5,18 @@ include("infra/db/connect.php");
 
 if(!isset($_SESSION["usuario"])){
     header("Location:  ../index.php");
-    exit(); //Está pequena parte do código serve para 
+    exit(); //Está pequena parte do código serve para se não existir um usuário autenticado ele não consegue entrar nas próximas páginas do site.
 }
 
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){ //Está parte serve para verifiicar se o usuario mandou algo, enviou o formulário ou algum dado.
 
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
 
     $sql = "INSERT INTO usuario(usuario, senha) VALUES ('$usuario','$senha')";
     
-    if($conn -> query($sql) === TRUE){
+    if($conn -> query($sql) === TRUE){ 
 
     echo "<script> alert('Usuario Cadastrado com sucesso')</script>";
 
@@ -26,6 +26,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     echo "<script> alert('Usuario não cadastrado com sucesso')</script>";
 
     }
+
+//Esta parte serve para conectar com o banco de dados e colocar em nossa tela um alert caso o funcionamento do login seja realizado com sucesso ou não.
 
 }
 ?>
@@ -41,7 +43,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
      <?php
-        include("../public/component/navbar.php");
+        include("../public/component/navbar.php"); 
+        
+        //Esta parte do código serve para colocar uma navbar dentro da página home, utilizando 
+        // o include de outra página assim deixando o código mais simples e fácil de ler e mais organizado.
     ?>
     
 <h2>Bem-Vindo!</h2>
