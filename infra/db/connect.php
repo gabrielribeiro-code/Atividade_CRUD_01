9ea355b3c2a377a586843c9217324a1bc1e07aa5
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-06-01 09:08:56 -0300

    docs: Comentário para explicar o funcionamento da página connect.php.

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -8,6 +8,8 @@ $db = "sistema_simpless";
 
 $conn = new mysqli($host,$user,$pass,$db);
 
+//Está parte serve ser uma conexão com as outras páginas para assim não ter que startat o php em todas as páginas deixando o código mais limpo e simples.
+
 
 // if($conn->connect_error){
   //  die("Erro na conexão");
